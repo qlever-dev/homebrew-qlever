c8a5859a0c58784e1171b18ade6841deec9d6a81
Tanmay Garg <tanmaygarg9879@gmail.com>
2026-01-18 23:29:01 +0100

    Add qlever-control formula

---
+++ Formula/qlever-control.rb
@@ -0,0 +1,84 @@
+class QleverControl < Formula
+  include Language::Python::Virtualenv
+
+  desc "Command-line interface for QLever SPARQL engine"
+  homepage "https://github.com/ad-freiburg/qlever-control"
+  url "https://files.pythonhosted.org/packages/fb/46/fc020063ce0168e40c7c9664782cb1f40bef3c08f0ee12999313e2fccc0f/qlever-0.5.42.tar.gz"
+  sha256 "0a0b91d44a391f6836419d4ed74bf71336ad845f61f0e7b8c508718182dc8a6d"
+  license "Apache-2.0"
+
+  depends_on "python@3.12"
+
+  resource "argcomplete" do
+    url "https://files.pythonhosted.org/packages/38/61/0b9ae6399dd4a58d8c1b1dc5a27d6f2808023d0b5dd3104bb99f45a33ff6/argcomplete-3.6.3.tar.gz"
+    sha256 "62e8ed4fd6a45864acc8235409461b72c9a28ee785a2011cc5eb78318786c89c"
+  end
+
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/e0/2d/a891ca51311197f6ad14a7ef42e2399f36cf2f9bd44752b3dc4eab60fdc5/certifi-2026.1.4.tar.gz"
+    sha256 "ac726dd470482006e014ad384921ed6438c457018f4b3d204aea4281258b2120"
+  end
+
+  resource "charset-normalizer" do
+    url "https://files.pythonhosted.org/packages/13/69/33ddede1939fdd074bce5434295f38fae7136463422fe4fd3e0e89b98062/charset_normalizer-3.4.4.tar.gz"
+    sha256 "94537985111c35f28720e43603b8e7b43a6ecfb2ce1d3058bbe955b73404e21a"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/6f/6d/0703ccc57f3a7233505399edb88de3cbd678da106337b9fcde432b65ed60/idna-3.11.tar.gz"
+    sha256 "795dafcc9c04ed0c1fb032c2aa73654d8e8c5023a7df64a53f39190ada629902"
+  end
+
+  resource "psutil" do
+    url "https://files.pythonhosted.org/packages/73/cb/09e5184fb5fc0358d110fc3ca7f6b1d033800734d34cac10f4136cfac10e/psutil-7.2.1.tar.gz"
+    sha256 "f7583aec590485b43ca601dd9cea0dcd65bd7bb21d30ef4ddbf4ea6b5ed1bdd3"
+  end
+
+  resource "pyparsing" do
+    url "https://files.pythonhosted.org/packages/33/c1/1d9de9aeaa1b89b0186e5fe23294ff6517fce1bc69149185577cd31016b2/pyparsing-3.3.1.tar.gz"
+    sha256 "47fad0f17ac1e2cad3de3b458570fbc9b03560aa029ed5e16ee5554da9a2251c"
+  end
+
+  resource "PyYAML" do
+    url "https://files.pythonhosted.org/packages/05/8e/961c0007c59b8dd7729d542c61a4d537767a59645b82a0b521206e1e25c2/pyyaml-6.0.3.tar.gz"
+    sha256 "d76623373421df22fb4cf8817020cbb7ef15c725b9d5e45f17e189bfc384190f"
+  end
+
+  resource "rdflib" do
+    url "https://files.pythonhosted.org/packages/ec/1b/4cd9a29841951371304828d13282e27a5f25993702c7c87dcb7e0604bd25/rdflib-7.5.0.tar.gz"
+    sha256 "663083443908b1830e567350d72e74d9948b310f827966358d76eebdc92bf592"
+  end
+
+  resource "requests" do
+    url "https://files.pythonhosted.org/packages/c9/74/b3ff8e6c8446842c3f5c837e9c3dfcfe2018ea6ecef224c710c85ef728f4/requests-2.32.5.tar.gz"
+    sha256 "dbba0bac56e100853db0ea71b82b4dfd5fe2bf6d3754a8893c3af500cec7d7cf"
+  end
+
+  resource "requests-sse" do
+    url "https://files.pythonhosted.org/packages/cf/73/dd6b0ae667c7720ddd5479f6216b1442610fdd162e27ce7bfb8357083f06/requests_sse-0.5.2.tar.gz"
+    sha256 "2bcb7cf905074b18ff9f7322716234c1188dfde805bba38300b37c6b5ae3a20a"
+  end
+
+  resource "termcolor" do
+    url "https://files.pythonhosted.org/packages/46/79/cf31d7a93a8fdc6aa0fbb665be84426a8c5a557d9240b6239e9e11e35fc5/termcolor-3.3.0.tar.gz"
+    sha256 "348871ca648ec6a9a983a13ab626c0acce02f515b9e1983332b17af7979521c5"
+  end
+
+  resource "tqdm" do
+    url "https://files.pythonhosted.org/packages/a8/4b/29b4ef32e036bb34e4ab51796dd745cdba7ed47ad142a9f4a1eb8e0c744d/tqdm-4.67.1.tar.gz"
+    sha256 "f8aef9c52c08c13a65f30ea34f4e5aac3fd1a34959879d7e59e63027286627f2"
+  end
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/c7/24/5f1b3bdffd70275f6661c76461e25f024d5a38a46f04aaca912426a2b1d3/urllib3-2.6.3.tar.gz"
+    sha256 "1b62b6884944a57dbe321509ab94fd4d3b307075e0c2eae991ac71ee15ad38ed"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/qlever --version")
+  end
+end
--- Formula/qlever.rb
+++ Formula/qlever.rb
@@ -4,34 +4,35 @@ class Qlever < Formula
   version "0.5.38"
   license "Apache-2.0"
   
-  url "https://github.com/tanmay-9/qlever/releases/download/v0.5.38/qlever-0.5.38-macos-arm64.tar.gz"
-  sha256 "4021173be61a69c37872652722b8959e9c52218f00821da26a9143c507e5db5f"
+  url "https://github.com/tanmay-9/qlever/releases/download/v0.5.42/qlever-0.5.42-macos-arm64.tar.gz"
+  sha256 "b6710e494bb3784854fd3d05a48b122c51f6916dc73cd0a30e4d7f8b0fa7e9bb"
 
-  depends_on "boost"                                                                                                                                                                                                                        
-  depends_on "icu4c@77"                                                                                                                                                                                                                     
-  depends_on "jemalloc"                                                                                                                                                                                                                     
-  depends_on "openssl@3" 
+  depends_on "boost"
+  depends_on "icu4c"
+  depends_on "jemalloc"
+  depends_on "openssl@3"
+  depends_on "qlever-control"
   depends_on arch: :arm64
   depends_on :macos
 
   def install
-    bin.install "qlever-loader"
-    bin.install "qlever-server"
+    bin.install "bin/qlever-index"
+    bin.install "bin/qlever-server"
   end
 
   def caveats
     <<~EOS
       QLever binaries have been installed:
-        - qlever-loader: for loading and indexing data
+        - qlever-index: for loading and indexing data
         - qlever-server: start a SPARQL endpoint and query data
+        - qlever: CLI tool for managing QLever instances
 
-      Use QLever via the qlever CLI (pip install qlever).
       For more information: https://github.com/ad-freiburg/qlever-control
     EOS
   end
 
   test do                                                                                                                                                                                                                                     
-    assert_match version.to_s, shell_output("#{bin}/qlever-loader --version")                                                                                                                                                                 
+    assert_match version.to_s, shell_output("#{bin}/qlever-index --version")                                                                                                                                                                 
     assert_match version.to_s, shell_output("#{bin}/qlever-server --version")                                                                                                                                                                 
   end 
 end
